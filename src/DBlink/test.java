4f6cda8968cc8b7b76306b6a24110e1dbd11bbf0
Benoit <benoit.leclerc@etu.iut-tlse3.fr>
2022-12-09 15:19:00 +0100

    oskour

--- src/DBlink/test.java
+++ src/DBlink/test.java
@@ -5,12 +5,9 @@ import java.util.List;
 public class test {
 	
 	public static void main(String[] args) {
-		List<Equipe> l = BDSelect.getListeEquipesFromTournoi(1);
-		System.out.println(l);
+		System.out.println(Joueur.getJoueurFromPseudo("Poko"));
 	}
 	
-	
-	alooooors
 	
 	
 }
