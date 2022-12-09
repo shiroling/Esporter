be667abefee49baf2ef6c8bd7bba6fe2bb678c48
JehanPhlp <93180306+JehanPhlp@users.noreply.github.com>
2022-12-09 22:14:40 +0100

    refonte graphique des cartes Tournoi + ajout du Hover

--- src/IHM/CarteTournois.java
+++ src/IHM/CarteTournois.java
@@ -1,24 +1,65 @@
 package IHM;
+import java.awt.Color;
+import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.GridLayout;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 import DBlink.Tournoi;
 
 public class CarteTournois extends Carte {
 	private Tournoi tournoi;
+	private JPanel panelDate;
+	private JPanel panelNom;
 	/**
 	 * Create the panel.
 	 */
 	public CarteTournois(Tournoi tournoi) {
-		super("Tournoi",tournoi.getNom());
+		super();
 		this.tournoi = tournoi;
-		JLabel lbldate = new JLabel("Du :"+ tournoi.getDateDebut()+" au "+tournoi.getDateFin());
-		lbldate.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		add(lbldate);
+		GridLayout gridLayout = (GridLayout) getLayout();
+		gridLayout.setRows(2);
+		
+		panelNom = new JPanel();
+		panelNom.setBackground(new Color(255,255,255));
+		add(panelNom);
+		panelNom.setLayout(new FlowLayout(FlowLayout.CENTER, 20, 30));
+		
+		JLabel lblNomTournoi = new JLabel("Rl pro LAN Series");
+		lblNomTournoi.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 20));
+		lblNomTournoi.setHorizontalAlignment(SwingConstants.CENTER);
+		panelNom.add(lblNomTournoi);
+		
+		panelDate = new JPanel();
+		panelDate.setBackground(new Color(255,255,255));
+		add(panelDate);
+		panelDate.setLayout(new FlowLayout(FlowLayout.CENTER, 20, 20));
+		
+		JLabel lblDateDebutFinTournoi = new JLabel("10/02/2022 -- 15/02/2022");
+		lblDateDebutFinTournoi.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 20));
+		panelDate.add(lblDateDebutFinTournoi);
+		
+		List<JPanel> listPanelAHover = new ArrayList<>();
+		listPanelAHover.add(panelDate);
+		listPanelAHover.add(panelNom);
+		super.addHoverPannels(listPanelAHover);
 	}
+	
 	public Tournoi getTournoi() {
 		return tournoi;
+	}
+	
+	public JPanel getPanelNomTournoi() {
+		return this.panelNom;
+	}
+	
+	public JPanel getPanelDateTournoi() {
+		return this.panelDate;
 	}
 
 	
