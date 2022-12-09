8ee0f5855a36c10928cd272b27cc31bbe58f38eb
Benoit <benoit.leclerc@etu.iut-tlse3.fr>
2022-12-09 16:02:48 +0100

    balalnsaljv faut pull

--- src/IHM/PanelRencontre.java
+++ src/IHM/PanelRencontre.java
@@ -3,12 +3,16 @@ package IHM;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import DBlink.Equipe;
+import DBlink.Joueur;
 import DBlink.Poule;
 import DBlink.Rencontre;
 import DBlink.Tournoi;
 
+import java.awt.Font;
 import java.awt.GridLayout;
 import javax.swing.JScrollPane;
+import javax.swing.SwingConstants;
 
 public class PanelRencontre extends JPanel {
 
@@ -21,7 +25,7 @@ public class PanelRencontre extends JPanel {
 		JPanel panelRencontre = new JPanel();
 		add(panelRencontre);
 		
-		JLabel lblNomRencontre = new JLabel(r.getTournoi().getNom());
+		JLabel lblNomRencontre = new JLabel("Rencontre du tournoi : "+r.getTournoi().getNom());
 		panelRencontre.add(lblNomRencontre);
 		
 		
@@ -29,10 +33,47 @@ public class PanelRencontre extends JPanel {
 		JPanel panelEquipe1 = new JPanel();
 		add(panelEquipe1);
 		
+		JScrollPane scrollE1 = new JScrollPane();
+		panelEquipe1.add(scrollE1);
+		
+		JPanel panelJoueurE1 = new JPanel();
+		scrollE1.setViewportView(panelJoueurE1);
+		panelJoueurE1.setLayout(new GridLayout(0, 1, 0, 0));
+		
+		Equipe tempE = r.getEquipes().get(0);
+		JLabel lbljoueur;
+		for (Joueur j : tempE.getListJoueur()) {
+			lbljoueur = new JLabel(j.getPseudo());
+			lbljoueur.setName("Joueur");
+			lbljoueur.setHorizontalAlignment(SwingConstants.LEFT);
+			lbljoueur.setFont(new Font("Tahoma", Font.PLAIN, 16));
+			lbljoueur.addMouseListener(AccueilV2.getMa());
+			panelJoueurE1.add(lbljoueur);
+		}
+		
+		
+		
 		JPanel panelEquipe2 = new JPanel();
 		add(panelEquipe2);
 		
+		JScrollPane scrollE2 = new JScrollPane();
+		panelEquipe2.add(scrollE2);
 
+		JPanel panelJoueurE2 = new JPanel();
+		scrollE2.setViewportView(panelJoueurE2);
+		panelJoueurE2.setLayout(new GridLayout(0, 1, 0, 0));
+
+		tempE = r.getEquipes().get(1);
+		for (Joueur j : tempE.getListJoueur()) {
+			lbljoueur = new JLabel(j.getPseudo());
+			lbljoueur.setName("Joueur");
+			lbljoueur.setHorizontalAlignment(SwingConstants.LEFT);
+			lbljoueur.setFont(new Font("Tahoma", Font.PLAIN, 16));
+			lbljoueur.addMouseListener(AccueilV2.getMa());
+			panelJoueurE2.add(lbljoueur);
+		}
+		
+		
 	}
 
 }
