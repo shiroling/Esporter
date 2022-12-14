8c56d18e857321c22115743aae1ecfb001eb5960
Benoit <benoit.leclerc@etu.iut-tlse3.fr>
2022-12-14 15:44:27 +0100

    faut pull

--- src/IHM/PopupRencontre.java
+++ src/IHM/PopupRencontre.java
@@ -22,25 +22,17 @@ public class PopupRencontre extends JDialog {
 	public PopupRencontre(Rencontre r) {
 		setBounds(100, 100, 450, 300);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
-		{
-			JPanel buttonPane = new JPanel();
-			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
-			getContentPane().add(buttonPane, BorderLayout.SOUTH);
-			{
-				JButton okButton = new JButton("OK");
-				okButton.setActionCommand("OK");
-				buttonPane.add(okButton);
-				getRootPane().setDefaultButton(okButton);
-			}
-			{
-				JButton cancelButton = new JButton("Cancel");
-				cancelButton.setActionCommand("Cancel");
-				buttonPane.add(cancelButton);
-			}
-		}
+		contentPanel.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panelHead = new JPanel();
+		contentPanel.add(panelHead, BorderLayout.NORTH);
+		
+		JPanel panelCorp = new JPanel();
+		contentPanel.add(panelCorp, BorderLayout.CENTER);
+		
+		
 	}
 
 }
