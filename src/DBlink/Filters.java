0ede77b91bc9158212252132a364fb3c0ea3e63e
shiroling <contact.shiroling@gmail.com>
2023-01-09 15:08:48 +0100

    Update Filters.java

--- src/DBlink/Filters.java
+++ src/DBlink/Filters.java
@@ -16,6 +16,7 @@ public class Filters {
 	
 	// Inscriptions
 	public static Predicate<Tournoi> sontInscriptionsFinies = t -> BDPredicats.sontInscriptionsFinies(t.getId());
+	public static Predicate<Tournoi> sontInscriptionsEnCours = Filters.sontInscriptionsFinies.negate();
 	
 	// Multijoueurs
 	public static Predicate<Tournoi> estTournoiMulti = t -> BDPredicats.estTournoiMulti(t);
@@ -48,6 +49,7 @@ public class Filters {
 	// Equipe
 	// Ecurie
 	public static BiPredicate<Equipe, Integer> estEquipeFromEcurie = (equipe, idEcurie) -> equipe.getIdEcurie() == idEcurie;
+	// Jeu
 	public static BiPredicate<Equipe, Integer> estEquipeSurJeu = (equipe, idJeu) -> equipe.getIdJeu() == idJeu;
 
 	
