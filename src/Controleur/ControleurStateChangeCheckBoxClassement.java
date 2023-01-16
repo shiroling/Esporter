b1ac32cd8f75231b7c66aac14a0757081ec73223
JehanPhlp <93180306+JehanPhlp@users.noreply.github.com>
2023-01-16 11:51:36 +0100

    jdujlvfjheojfbh cifhfjz "fgkjig" fgj

--- src/Controleur/ControleurStateChangeCheckBoxClassement.java
+++ src/Controleur/ControleurStateChangeCheckBoxClassement.java
@@ -1,5 +1,34 @@
 package Controleur;
 
-public class ControleurStateChangeCheckBoxClassement {
+import java.util.List;
+
+import javax.swing.JCheckBox;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+import DBlink.BDSelect;
+import DBlink.Equipe;
+import IHM.AccueilV2;
+
+public class ControleurStateChangeCheckBoxClassement implements ChangeListener{
+	
+	private AccueilV2 vue;
+	
+	public ControleurStateChangeCheckBoxClassement(AccueilV2 vue) {
+		super();
+		this.vue = vue;
+	}
+
+	@Override
+	public void stateChanged(ChangeEvent e) {
+		JCheckBox check = (JCheckBox) e.getSource();
+		
+		if(check.isSelected()) {
+			this.vue.viderCartes();
+			
+			List<Equipe> equipes = BDSelect.getClassementGeneral();
+			this.vue.ajouterCartesEquipe(equipes);
+		}
+	}
 
 }
