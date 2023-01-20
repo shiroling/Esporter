ec0ee4de6d32f1796694591223256ac29b20907b
NoaDespaux <noa.despaux@etu.iut-tlse3.fr>
2023-01-20 12:20:20 +0100

    clic marche

--- src/controleur_ihm/mouthAdapteruAccueil.java
+++ src/controleur_ihm/mouthAdapteruAccueil.java
@@ -3,7 +3,12 @@ package controleur_ihm;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
-import controleur_ihm.ControleurAccueil.Etat;
+import javax.swing.JLabel;
+
+import contoleur_bd.Ecurie;
+import contoleur_bd.Equipe;
+import contoleur_bd.Joueur;
+import contoleur_bd.Tournoi;
 import interfaces.Accueil;
 import interfaces.CarteEcurie;
 import interfaces.CarteEquipe;
@@ -24,22 +29,40 @@ public class mouthAdapteruAccueil extends MouseAdapter {
 	public void mouseClicked(MouseEvent e) {
 		ControleurAccueil controleur = vue.getControleur();
 		Object obj = e.getSource();
-			if (obj instanceof CarteEcurie) {
-				CarteEcurie ce = (CarteEcurie) obj;
-				vue.procedureCreePopupEcurie(ce.getEcurie());
-			} else if (obj instanceof CarteTournois) {
-				CarteTournois ct = (CarteTournois) obj;
-				vue.procedureCreePopupTournoi(ct.getTournoi());
-			} else if (obj instanceof CarteEquipe) {
-				CarteEquipe ce = (CarteEquipe) obj;
-				vue.procedureCreePopupEquipe(ce.getEquipe());
-			} else if (obj instanceof CarteJeu) {
-				CarteJeu ce = (CarteJeu) obj;
-				vue.procedureCreePopupJeu(ce.getJeu());
-			} else if (obj instanceof CarteRencontre) {
-				CarteRencontre cr = (CarteRencontre) obj;
-				vue.procedureCreePopupRencontre(cr.getRencontre());
+		if (obj instanceof CarteEcurie) {
+			CarteEcurie ce = (CarteEcurie) obj;
+			vue.procedureCreePopupEcurie(ce.getEcurie());
+		} else if (obj instanceof CarteTournois) {
+			CarteTournois ct = (CarteTournois) obj;
+			vue.procedureCreePopupTournoi(ct.getTournoi());
+		} else if (obj instanceof CarteEquipe) {
+			CarteEquipe ce = (CarteEquipe) obj;
+			vue.procedureCreePopupEquipe(ce.getEquipe());
+		} else if (obj instanceof CarteJeu) {
+			CarteJeu ce = (CarteJeu) obj;
+			vue.procedureCreePopupJeu(ce.getJeu());
+		} else if (obj instanceof CarteRencontre) {
+			CarteRencontre cr = (CarteRencontre) obj;
+			vue.procedureCreePopupRencontre(cr.getRencontre());
+		} else if (obj instanceof JLabel) {
+			JLabel jl = (JLabel) obj;
+			switch (jl.getName()) {
+			case "Joueur":
+				vue.procedureCreerPopup(Joueur.getJoueurFromPseudo(jl.getText()), controleur);
+				break;
+			case "Equipe":
+				vue.procedureCreerPopup(Equipe.getEquipeFromNom(jl.getText()), controleur);
+				break;
+			case "Ecurie":
+				vue.procedureCreerPopup(Ecurie.getEcurieFromNom(jl.getText()), controleur);
+				break;
+			case "Tournoi":
+				vue.procedureCreerPopup(Tournoi.getTournoiFromNom(jl.getText()), controleur);
+				break;
+			default:
+				break;
 			}
-		
+		}
 
-}};
+	}
+};
